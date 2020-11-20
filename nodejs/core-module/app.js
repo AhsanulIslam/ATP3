cb232ffc495db668b555ed27c88a129bebdd2af5
Ahsanulislam <imon.ahsanul.islam@gmail.com>
2020-11-20 12:08:05 +0600

    second commit

--- nodejs/core-module/app.js
+++
@@ -1,7 +0,0 @@
-const fs = require ('fs');
-
-// fs.writeFileSync('data.txt','this is fs core module....');
-// console.log('done')
-
-var data =fs.readFileSync('data.txt');
-console.log(data.toString())
